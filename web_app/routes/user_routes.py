70e549cdffe37788fd8e1386ca3c1d8c5e74cf73
MJ Rossetti <s2t2@users.noreply.github.com>
2022-03-29 15:06:56 -0400

    Remove unused code

--- web_app/routes/user_routes.py
+++ web_app/routes/user_routes.py
@@ -59,35 +59,3 @@ def profile():
     current_user = session.get("current_user")
     #user = fetch_user(email=current_user["email"])
     return render_template("user_profile.html", user=current_user) # user=user
-
-
-#@user_routes.route("/user/profile/edit")
-#@authenticated_route
-#def edit_profile():
-#    print("EDIT PROFILE...")
-#    current_user = session.get("current_user")
-#    #user = fetch_user(email=current_user["email"])
-#    return render_template("user_profile.html", user=current_user) # user=user
-
-#@user_routes.route("/user/profile/update", methods=["POST"])
-#@authenticated_route
-#def update_profile():
-#    #print("UPDATE PROFILE...")
-#
-#    form_data = dict(request.form)
-#    print("FORM DATA:", form_data)
-#    #full_name = form_data["full_name"]
-#    first_name = form_data["first_name"]
-#    last_name = form_data["last_name"]
-#
-#    current_user = session.get("current_user")
-#    user = User.query.filter_by(email=current_user["email"]).first()
-#    #user.full_name = full_name
-#    user.first_name = first_name
-#    user.last_name = last_name
-#    db.session.add(user)
-#    db.session.commit()
-#
-#    flash("Profile updated!", "success")
-#    return render_template("user_profile.html", user=user)
-#
