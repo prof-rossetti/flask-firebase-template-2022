ef9a12ebb7e81e05fefd8eeebfae1f3600f7396d
MJ Rossetti <s2t2@users.noreply.github.com>
2022-03-29 14:44:40 -0400

    Basic tests

---
+++ test/test_web_app.py
@@ -0,0 +1,33 @@
+
+import pytest
+
+from web_app import create_app
+
+# see: https://flask.palletsprojects.com/en/2.1.x/testing/
+
+@pytest.fixture(scope="module")
+def test_client():
+    app = create_app()
+    app.config.update({"TESTING": True})
+    return app.test_client()
+
+
+def test_home(test_client):
+    response = test_client.get("/")
+    assert response.status_code == 200
+    assert b"<h1>Home</h1>" in response.data
+
+def test_about(test_client):
+    response = test_client.get("/about")
+    assert response.status_code == 200
+    assert b"<h1>About</h1>" in response.data
+
+def test_products(test_client):
+    response = test_client.get("/products")
+    assert response.status_code == 200
+    assert b"<h1>Products</h1>" in response.data
+
+
+    assert b"Textbook" in response.data
+    assert b"Cup of Tea" in response.data
+    assert b"Strawberries" in response.data
